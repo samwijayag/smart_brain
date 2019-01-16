301bf3af5c586ae8cd0020760a5a76730aa7a36e
samwijayag <sam.wiajayg@gmail.com>
2019-01-16 14:01:07 -0800

    added particles

---
+++ src/Components/Rank/Rank.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+
+const Rank = () => {
+	return(
+		<div>
+			<div className='White f3'>
+				{'Sam, Your current rank is...'}
+			</div>
+			<div className='White f1'>
+				{'#3'}
+			</div>
+		</div>
+	)
+}
+
+export default Rank;
