0c8ae670683c2fab2888da18a01162fbe9a3f329
samwijayag <sam.wijayag@gmail.com>
2019-01-16 23:53:57 -0800

    added api

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import React, { Component } from 'react';
+import Clarifai from 'clarifai'
+import Particles from 'react-particles-js'
 import './App.css';
 import Navigation from './Components/Navigation/Navigation'
 import Logo from './Components/Logo/Logo'
 import ImageLinkForm from './Components/ImageLinkForm/ImageLinkForm'
 import Rank from './Components/Rank/Rank'
-import Particles from 'react-particles-js'
+import FaceRecognition from './Components/FaceRecognition/FaceRecognition'
+
+
+const app = new Clarifai.App({
+ apiKey: '8e03116228814a758a92cf4492480554'
+});
 
 
 const particlesOptions = {	
@@ -19,21 +26,52 @@ const particlesOptions = {
 		}
 }
 class App extends Component {
-  render() {
-    return (
-      <div className='App'>
-	      <div>
-	      	<Particles className='particles'
-	              params={particlesOptions}
-	              />
-	        <Navigation />
-	        <Logo />
-	        <Rank />
-	        <ImageLinkForm />
+	constructor(){
+		super();
+		this.state = {
+			input: '',
+			imageUrl: ''
+		}
+	}
+
+	onInputChange = (event) => {
+		this.setState({input: event.target.value});
+	}
+
+	onButtonSubmit = () => {
+		this.setState({imageUrl : this.state.input})
+		app.models.predict(
+			Clarifai.COLOR_MODEL, 
+			this.state.input)
+		.then(
+		    function(response) {
+		    	console.log(response)
+		      // do something with response
+		    },
+		    function(err) {
+		      // there was an error
+		    }
+		  );
+	}
+
+	render() {
+	    return (
+	      <div className='App'>
+		      <div>
+		      	<Particles className='particles'
+		              params={particlesOptions}
+		              />
+		        <Navigation />
+		        <Logo />
+		        <Rank />
+		        <ImageLinkForm onInputChange ={this.onInputChange}
+		       	 onButtonSubmit={this.onButtonSubmit}
+		       	 />
+		       	<FaceRecognition imageUrl={this.state.imageUrl}/>
+		      </div>
 	      </div>
-      </div>
-    )
-  }
+	    )
+	  }
 }
 
 export default App;
