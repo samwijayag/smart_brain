64912db8465a75c789de78c6da2b67f49c15885f
samwijayag <sam.wijayag@gmail.com>
2019-01-26 10:06:07 -0800

    backend connected

--- src/App.js
+++ src/App.js
@@ -35,8 +35,28 @@ class App extends Component {
 			imageUrl: '', 
 			box:{},
 			route: 'signin',
-			isSignedIn: false
+			isSignedIn: false,
+			user: {
+				id:'',
+				email:'',
+				name: '', 
+				entries:0 ,
+				joined: ''
+
+			}
 		}
+	}
+
+	loadUser = (data) => {
+		 this.setState({
+		 	user:{
+		 		id:data.id,
+				email:data.email,
+				name:data.name, 
+				entries:data.entries,
+				joined:data.joined
+			}
+		 })
 	}
 
 	calculateFaceLocation = (data) => {
@@ -61,15 +81,32 @@ class App extends Component {
 		this.setState({input: event.target.value});
 	}
 
-	onButtonSubmit = () => {
-		this.setState({imageUrl : this.state.input})
-		app.models
-		.predict(
-			Clarifai.FACE_DETECT_MODEL, 
-			this.state.input)
-		.then(response => this.displayFaceBox(this.calculateFaceLocation(response)))
-		.catch(err => console.log(err));
-	}
+	
+  onButtonSubmit = () => {
+    this.setState({imageUrl: this.state.input});
+    app.models
+      .predict(
+        Clarifai.FACE_DETECT_MODEL,
+        this.state.input)
+      .then(response => {
+        if (response) {
+          fetch('http://localhost:3000/image', {
+            method: 'put',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify({
+              id: this.state.user.id
+            })
+          })
+            .then(response => response.json())
+            .then(count => {
+              this.setState(Object.assign(this.state.user, { entries: count}))
+            })
+
+        }
+        this.displayFaceBox(this.calculateFaceLocation(response))
+      })
+      .catch(err => console.log(err));
+  }
 
 	onRouteChange = (route) => {
 		if(route=== 'signout'){
@@ -101,7 +138,9 @@ class App extends Component {
 			        : (
 			        	this.state.route === 'signin' 
 			        	? <Signin onRouteChange={this.onRouteChange}/>
-			        	: <Register onRouteChange={this.onRouteChange}/>
+			        	: <Register 
+			        	loadUser = {this.loadUser}
+			        	onRouteChange={this.onRouteChange}/>
 
 			        ) 
 			       
